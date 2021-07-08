1eb34a9260d4bdb809f2e95f06dc5f930c7bbf9b
ArifulShanto <codinglife1998@gmail.com>
2021-07-08 18:17:36 +0600

    some works based on vanilla js

---
+++ assignment.js
@@ -0,0 +1,60 @@
+function feetToMile(feet){
+    if(feet<0){
+        return 'Negative distance is not possible';
+    }
+    else{
+        var result = feet/5280;
+        return result;
+    }
+}
+var test = feetToMile(300);
+console.log(test);
+
+function woodCalculator(chair,table,bed){
+    if(chair < 0 || table <0 || bed < 0){
+        return 'Negative number of furniture is not possible';
+    }
+
+    var total = (chair*1)+(table*3)+(bed*5);
+    return total;
+}
+
+var totalWood = woodCalculator(11,1,1);
+console.log(totalWood);
+
+function brickCalculator(totalFloor){
+    if(totalFloor < 0){
+        return 'Negative floor is not possible';
+    }
+    if(totalFloor<=10){
+        var totalBrick = n*15*1000;
+        return totalBrick
+    }
+    else if(10<totalFloor<=20){
+        var totalBrick = (10*15*1000) + ((n-10)*12*1000);
+        return totalBrick;
+    }
+    else{
+        var totalBrick = (10*15*1000) + (10*12*1000) + ((n-20)*10*1000);
+        return totalBrick;
+    }
+}
+
+var building = brickCalculator(-7);
+console.log(building);
+
+function tinyFriend(friends){
+    var tinyName = friends[0];
+    var min = tinyName.length;
+    for(var i = 0 ; i<friends.length;i++){
+        var afriend = friends[i];
+        if(afriend.length<=min){
+            min = afriend.length;
+            tinyName = friends[i];
+        }
+    }
+    return tinyName ;
+}
+
+var tinyFriendName = tinyFriend(['S','Shan','Joe']);
+console.log(tinyFriendName);
